b98a6a6872e56afc7fcd451360bc4e37915190c9
Shatlykch <157189400+Shatlykch@users.noreply.github.com>
2024-03-13 12:27:44 -0400

    deploy ready

--- NASA_API_PROJECT/db/connection.js
+++ NASA_API_PROJECT/db/connection.js
@@ -1,10 +1,11 @@
 import mongoose from "mongoose";
 
-const connectionString = process.env.DB_URL || "mongodb://localhost/apod_api"
+const connectionString =
+  process.env.DB_URL || "mongodb://127.0.0.1:27017/apod_api"
 
 mongoose.set("returnOriginal", false);
 
-mongoose.connect("mongodb://127.0.0.1:27017/apod_api").catch((err) => {
+mongoose.connect(connectionString).catch((err) => {
   console.log(`Error connection to MongoDB: ${err.message}`);
 });
 
